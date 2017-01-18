446e07b3bd2bee5210e73492f71f5e5198c23cd3
Dmitry Komkov <dmitry.komkov@acronis.com>
2017-01-18 19:25:02 +0300

    Add basic blog view

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,11 @@
-from django.conf.urls import url
-from . import views
+from django.conf.urls import url, include
+from django.views.generic import ListView, DetailView
+from blog.models import Post
+#from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name = "index"),
+    url(r'^$',
+        ListView.as_view(queryset = Post.objects.all().order_by("-date")[:10],
+        template_name = "blog/blog.html")
+    ),
 ]
