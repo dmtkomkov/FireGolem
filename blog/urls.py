8f5eb6f861c4e39bcafe4b80fa4ca3736fda416e
Dmitry Komkov <dmitry.komkov@acronis.com>
2017-01-18 20:18:14 +0300

    Add individual blog page view

--- blog/urls.py
+++ blog/urls.py
@@ -6,6 +6,12 @@ from blog.models import Post
 urlpatterns = [
     url(r'^$',
         ListView.as_view(queryset = Post.objects.all().order_by("-date")[:10],
-        template_name = "blog/blog.html")
+            template_name = "blog/blog.html"
+        )
+    ),
+    url(r'^(?P<pk>\d+)$',
+        DetailView.as_view(model = Post,
+            template_name = "blog/post.html"
+        )
     ),
 ]
