ab9f09f08fa39944919897c0eb4907de14b17000
Dmitry <drnecromant@gmail.com>
2017-01-18 11:36:36 +0300

    Add contact page with arguments

--- personal/views.py
+++ personal/views.py
@@ -2,3 +2,10 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'personal/home.html')
+
+def contact(request):
+    content = [
+        'Hello, that is my contact page',
+        'Contact me if you can'
+    ]
+    return render(request, 'personal/basic.html', {'content': content})
