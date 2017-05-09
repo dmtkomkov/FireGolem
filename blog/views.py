3d7336391759f4f013577f01b5216e0c2b192a2e
Dmitry <drnecromant@gmail.com>
2017-05-09 04:06:01 +0300

    Major changes for overall design

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.http import HttpResponse
+from blog.models import Post
+
 
 def index(request):
     return HttpResponse("<h2>Hello World!</h2>")
+
+
+def post(request):
+    data = request.POST
+    title = data.get('title')
+    post = data.get('post')
+    print(title, post)
+    db_post = Post(title=title, body=post)
+    db_post.save()
+    return redirect('/blog/', permanent=True)
--- personal/views.py
+++ personal/views.py
@@ -2,10 +2,3 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'personal/home.html')
-
-def contact(request):
-    content = [
-        'Hello, that is my contact page',
-        'Contact me if you can'
-    ]
-    return render(request, 'personal/basic.html', {'content': content})
