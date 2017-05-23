54eecda8ed5e3f37f125c42770cda7e4199620f1
Dmitry <drnecromant@gmail.com>
2017-05-23 16:06:58 +0300

    Added pagination for blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,7 @@
-from django.conf.urls import url, include
-from django.views.generic import ListView, DetailView
-from blog.models import Post
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',
-        ListView.as_view(queryset=Post.objects.all().order_by("-date")[:10],
-            template_name="blog/blog.html"
-        )
-    ),
+    url(r'^$', views.index, name="index"),
     url(r'^post/$', views.post, name="post")
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,62 @@
 from django.shortcuts import redirect
-from django.http import HttpResponse
+from django.shortcuts import render
 from blog.models import Post
+from django.core.paginator import Paginator, EmptyPage
+
+
+PPP = 10 # POSTS_PER_PAGE
+PML = 11 # PAGINATOR_MAX_LENGTH
+PHL = (PML - 1) // 2 # PAGINATOR_HALF_LENGTH
 
 
 def index(request):
-    return HttpResponse("<h2>Hello World!</h2>")
+    all_posts = Post.objects.all().order_by("date")
+    paginator = Paginator(all_posts, PPP)
+    active_page = request.GET.get('page')
+
+    try:
+        active_page = int(active_page)
+        posts = paginator.page(active_page)
+    except (ValueError, TypeError, EmptyPage): # get the last page in case of wrong active page
+        active_page = paginator.num_pages
+        posts = paginator.page(active_page)
+
+    if paginator.num_pages <= PML: # num_pages less than maximum
+        page_numbers = range(1, paginator.num_pages + 1)
+    elif active_page <= PHL:
+        page_numbers = range(1, PML + 1)
+    elif active_page >= paginator.num_pages - PHL:
+        page_numbers = range(paginator.num_pages - PML + 1, paginator.num_pages + 1)
+    else:
+        page_numbers = range(active_page - PHL, active_page + PHL + 1)
+
+    # next and prev button references on the pagination bar
+    if active_page == 1:
+        prev_page = None
+    elif active_page <= PML:
+        prev_page = 1
+    else:
+        prev_page = active_page - PML
+    if active_page == paginator.num_pages:
+        next_page = None
+    elif active_page >= paginator.num_pages - PML:
+        next_page = paginator.num_pages
+    else:
+        next_page = active_page + PML
+
+    return render(request, 'blog/blog.html',
+                  {'posts': posts,
+                   'active_page': active_page,
+                   'page_numbers': page_numbers,
+                   'prev_page': prev_page,
+                   'next_page': next_page
+                   })
 
 
 def post(request):
     data = request.POST
     title = data.get('title')
     post = data.get('post')
-    print(title, post)
     db_post = Post(title=title, body=post)
     db_post.save()
     return redirect('/blog/', permanent=True)
