683ed38d23b9c2f930e8455bb907c6c515fa2114
Dmitry Komkov <drnecromant@gmail.com>
2017-07-31 23:56:45 +0200

    Use local mysql instead of sqlite db

--- myweb/settings.py
+++ myweb/settings.py
@@ -77,6 +77,13 @@ WSGI_APPLICATION = 'myweb.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blog',
+        'USER': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    },
+    'sqlite': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
