7c336f940c319823c039ae309076b7d9cef1bac8
Dmitry Komkov <drnecromant@gmail.com>
2017-08-10 20:26:21 +0200

    Created custom paginator class
    It returns page_count, page_number and does not return links

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from api import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    url(r'^post/', views.PostViewSet.as_view()),
+    url(r'^post/', views.PostList.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
--- api/views.py
+++ api/views.py
@@ -2,14 +2,26 @@ from blog.models import Post
 from rest_framework import generics, mixins
 from api.serializers import PostSerializer
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
-class PostViewSet(generics.GenericAPIView,
+class CustomPagination(PageNumberPagination):
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'page_size': self.page_size,
+            'page_number': self.page.number,
+            'page_count': self.page.paginator.num_pages,
+            'results': data
+        })
+
+
+class PostList(generics.GenericAPIView,
                   mixins.ListModelMixin,
                   mixins.CreateModelMixin):
     queryset = Post.objects.all().order_by('date')
     serializer_class = PostSerializer
-    pagination_class = PageNumberPagination
+    pagination_class = CustomPagination
 
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
