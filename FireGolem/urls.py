16e663c4633d464bd5178f3c5ae94e690eb705f3
Dmitry Komkov <drnecromant@gmail.com>
2017-08-15 00:32:53 +0200

    Return back admin page

--- FireGolem/urls.py
+++ FireGolem/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import url, include
+from django.contrib import admin
 
 # TODO: add namespaces for different applications
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
     url(r'^', include('home.urls')),
     url(r'^blog/', include('blog.urls')),
