933e90e83b652878597103dfdf6fbb038a821f07
Dmitry Komkov <drnecromant@gmail.com>
2017-08-22 12:30:26 +0200

    Small fix for wsgi configuration
    For apache could find django settings file

--- FireGolem/wsgi.py
+++ FireGolem/wsgi.py
@@ -1,6 +1,8 @@
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/var/www/FireGolem')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "FireGolem.settings")
 
