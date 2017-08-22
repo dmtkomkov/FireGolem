824d93ce199a2fbac84b61dfb5327d319b585f4e
Dmitry Komkov <drnecromant@gmail.com>
2017-08-22 14:36:44 +0200

    Remove sqlite configuration and comments from settings

--- FireGolem/settings.py
+++ FireGolem/settings.py
@@ -1,12 +1,5 @@
-import os
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '#aat!fl%wz4+ggzj9(15e3*ryac5&h4(3r+z1*_v!*v*a&^gw!'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = []
@@ -82,10 +75,6 @@ DATABASES = {
         'USER': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
-    },
-    'sqlite': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
