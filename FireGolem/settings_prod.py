4cbea30c592789d7ef85670fd5eb4e713fd96818
Dmitry Komkov <drnecromant@gmail.com>
2017-08-31 08:48:31 +0200

    Add logger to production config

--- FireGolem/settings_prod.py
+++ FireGolem/settings_prod.py
@@ -5,3 +5,19 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 DEBUG = False
 
 STATIC_ROOT = '/var/www/FireGolem/static/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        },
+    },
+}
