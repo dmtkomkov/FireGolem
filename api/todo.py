30c1d810863c348b02dce8c9f7079e3152d39b75
Dmitry Komkov <drnecromant@gmail.com>
2017-09-28 18:04:35 +0200

    Updated todolist

--- api/todo.py
+++
@@ -1,8 +0,0 @@
-# TODO: Return csrf token and do not replace delete and put method with middleware
-# TODO: Django forms
-# TODO: Bootstrap form group to align buttons together
-# TODO: Length limit for title and body text
-
-# TODO: add defense from brute force
-# TODO: Add icon for site
-# TODO: Add tests
---
+++ api/todolist.py
@@ -0,0 +1,17 @@
+# Temporarily storage for todo list
+
+# Generic tasks
+# TODO: Return csrf token and do not replace delete and put method with middleware
+# TODO: Django forms
+# TODO: Bootstrap form group to align buttons together
+# TODO: Length limit for title and body text
+# TODO: tag_inclusion for dialog
+# TODO: use real api
+# TODO: Add tests
+# TODO: Add logging and Exception printing
+
+# Public web-site tasks
+# TODO: add defense from brute force
+# TODO: Add icon for site
+# TODO: DB backup-restore script
+# TODO: Set new db password with automation
