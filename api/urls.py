397978fd88a13f62d9f6342fabc7203efaf5a209
Dmitry Komkov <drnecromant@gmail.com>
2017-09-28 18:02:28 +0200

    Add name for api blog url

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from api import views
 
 urlpatterns = [
-    url(r'^blog/', views.PostList.as_view()),
+    url(r'^blog/', views.PostList.as_view(), name='blog'),
 ]
