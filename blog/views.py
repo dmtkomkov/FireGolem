390c7dc42ff218f435ae7360a43278c7cb424008
Dmitry Komkov <drnecromant@gmail.com>
2017-10-14 16:44:57 +0200

    Moved pagination job under helpers routine

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
-from django.core.paginator import Paginator, EmptyPage
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from api.models import Post
+from helpers.pagination import get_page
 
 PPP = 10  # POSTS_PER_PAGE
 PML = 11  # PAGINATOR_MAX_LENGTH
@@ -13,46 +13,13 @@ PHL = (PML - 1) // 2 # PAGINATOR_HALF_LENGTH
 class BlogView(LoginRequiredMixin, View):
     def get(self, request):
         all_posts = Post.objects.all().filter(deleted=False).order_by("created")
-        paginator = Paginator(all_posts, PPP)
         active_page = request.GET.get('page')
 
-        try:
-            active_page = int(active_page)
-            posts = paginator.page(active_page)
-        except (ValueError, TypeError, EmptyPage): # get the last page in case of wrong active page
-            active_page = paginator.num_pages
-            posts = paginator.page(active_page)
+        posts, page_conf = get_page(all_posts, active_page)
+        page = {'posts': posts}
+        page.update(page_conf)
 
-        if paginator.num_pages <= PML: # num_pages less than maximum
-            page_numbers = range(1, paginator.num_pages + 1)
-        elif active_page <= PHL:
-            page_numbers = range(1, PML + 1)
-        elif active_page >= paginator.num_pages - PHL:
-            page_numbers = range(paginator.num_pages - PML + 1, paginator.num_pages + 1)
-        else:
-            page_numbers = range(active_page - PHL, active_page + PHL + 1)
-
-        # next and prev button references on the pagination bar
-        if active_page == 1:
-            prev_page = None
-        elif active_page <= PML:
-            prev_page = 1
-        else:
-            prev_page = active_page - PML
-        if active_page == paginator.num_pages:
-            next_page = None
-        elif active_page >= paginator.num_pages - PML:
-            next_page = paginator.num_pages
-        else:
-            next_page = active_page + PML
-
-        return render(request, 'blog/home.html',
-                      {'posts': posts,
-                       'active_page': active_page,
-                       'page_numbers': page_numbers,
-                       'prev_page': prev_page,
-                       'next_page': next_page
-                       })
+        return render(request, 'blog/home.html', page)
 
     def post(self, request):
         title = request.POST.get('title')
--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,3 @@
-from django.core.paginator import Paginator, EmptyPage
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
@@ -6,6 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import QueryDict
 
 from api.models import Area, Project, Task, TaskStatus
+from helpers.pagination import get_page
 
 PPP = 10  # POSTS_PER_PAGE
 PML = 11  # PAGINATOR_MAX_LENGTH
@@ -41,50 +41,17 @@ class AreaView(LoginRequiredMixin, View):
         area.save()
         return self.get(request)
 
+
 class TodoView(LoginRequiredMixin, View):
     def get(self, request):
         all_todos = Task.objects.all().filter(deleted=False).order_by("updated")
-
-        paginator = Paginator(all_todos, PPP)
         active_page = request.GET.get('page')
 
-        try:
-            active_page = int(active_page)
-            todos = paginator.page(active_page)
-        except (ValueError, TypeError, EmptyPage):  # get the last page in case of wrong active page
-            active_page = paginator.num_pages
-            todos = paginator.page(active_page)
+        todos, page_conf = get_page(all_todos, active_page)
+        page = {'todos': todos}
+        page.update(page_conf)
 
-        if paginator.num_pages <= PML:  # num_pages less than maximum
-            page_numbers = range(1, paginator.num_pages + 1)
-        elif active_page <= PHL:
-            page_numbers = range(1, PML + 1)
-        elif active_page >= paginator.num_pages - PHL:
-            page_numbers = range(paginator.num_pages - PML + 1, paginator.num_pages + 1)
-        else:
-            page_numbers = range(active_page - PHL, active_page + PHL + 1)
-
-        # next and prev button references on the pagination bar
-        if active_page == 1:
-            prev_page = None
-        elif active_page <= PML:
-            prev_page = 1
-        else:
-            prev_page = active_page - PML
-        if active_page == paginator.num_pages:
-            next_page = None
-        elif active_page >= paginator.num_pages - PML:
-            next_page = paginator.num_pages
-        else:
-            next_page = active_page + PML
-
-        return render(request, 'todo/home.html',
-                      {'todos': todos,
-                       'active_page': active_page,
-                       'page_numbers': page_numbers,
-                       'prev_page': prev_page,
-                       'next_page': next_page
-                       })
+        return render(request, 'todo/home.html', page)
 
     def post(self, request):
         name = request.POST.get('name')
