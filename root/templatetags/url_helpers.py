cd914c77557b47bf6ba5c16c84030a04b1f32f81
Dmitry Komkov <drnecromant@gmail.com>
2017-11-05 20:47:11 +0100

    Badges improvement
    Add tail to badge
    Add Estimation badge

--- root/templatetags/url_helpers.py
+++ root/templatetags/url_helpers.py
@@ -8,3 +8,10 @@ def set_url_option(request, field, value):
     dict_ = request.GET.copy()
     dict_[field] = value
     return dict_.urlencode()
+
+
+@register.filter
+def tail(value, key):
+    if value is not None:
+        return str(value).split(key)[-1]
+    return value
