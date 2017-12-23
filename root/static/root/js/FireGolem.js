3671d29a45f7efb29bb3d60c2a7a43b648fd203a
Dmitry Komkov <drnecromant@gmail.com>
2017-12-24 00:37:05 +0100

    Fixed bug with extended post out of scroll

--- root/static/root/js/FireGolem.js
+++ root/static/root/js/FireGolem.js
@@ -4,24 +4,32 @@ $(document).ready(function() {
     $("#card-body").niceScroll();
 
     $('.panel.panel-blog').on('shown.bs.collapse', function () {
+        // Get information from expanded post
         post = $('[aria-expanded=true]');
         post_id = post.attr('post-id');
         post_title = post.find('.post-title')[0].innerText;
         post_body = post.find('.post-body')[0].innerText;
 
+        // Prepare data for editing by adding it to hidden input
         $('input.hidden[name="post_id"]').attr('value', post_id);
         $('input.post-title[name="title"]').attr('value', post_title);
         $('textarea.post-body[name="body"]').val(post_body);
 
+        // Enable control buttons
         $('.btn[data-target="#deletePost"]').prop('disabled', false);
         $('.btn[data-target="#editPost"]').prop('disabled', false);
+
+        $("#card-body").getNiceScroll().resize();
     });
 
     $('.panel.panel-blog').on('hidden.bs.collapse', function () {
         if ($('[aria-expanded=true]').length == 0) {
+            // Disable control buttons
             $('.btn[data-target="#deletePost"]').prop('disabled', true);
             $('.btn[data-target="#editPost"]').prop('disabled', true);
         }
+
+        $("#card-body").getNiceScroll().resize();
     });
 
     $('.panel.panel-sidebar').on('shown.bs.collapse', function () {
