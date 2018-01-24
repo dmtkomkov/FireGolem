7e2109d92067f13f936335894aa3ca6da5d05c78
Dmitry Komkov <drnecromant@gmail.com>
2018-01-24 04:07:27 +0100

    Modify some data on Dashboard and bring new one

--- root/views.py
+++ root/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from api.models import Post, Task, WorkLog
+from api.models import Post, Task, WorkLog, Payment
 from django.db.models import Sum
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 
 
 class RootView(LoginRequiredMixin, View):
     def get(self, request):
         blogStats = {}  # Build data for blog dashboard
 
-        blogStats['count'] = Post.objects.count()
+        blogStats['count'] = Post.objects.filter(worklog__isnull=True).count()
 
         if blogStats['count'] > 0:
-            latestPost = Post.objects.order_by('-created')[0]
+            latestPost = Post.objects.filter(worklog__isnull=True).order_by('-created')[0]
             daysDelta = (datetime.today() - latestPost.created).days
             if not daysDelta:
                 blogStats['latest'] = "today"
@@ -22,7 +22,7 @@ class RootView(LoginRequiredMixin, View):
         else:
             blogStats['latest'] = "No posts"
 
-        todoStats ={}  # Build data for todo dashboard
+        todoStats = {}  # Build data for todo dashboard
 
         currentLogs = WorkLog.objects.filter(task__status=2).filter(post__deleted=False)
         currentLogged = currentLogs.aggregate(Sum('log'))['log__sum'] or 0
@@ -34,7 +34,16 @@ class RootView(LoginRequiredMixin, View):
         latestLogs = WorkLog.objects.filter(post__deleted=False).filter(post__created__gte=weekAgo)
         todoStats['lastWeekLogged'] = latestLogs.aggregate(Sum('log'))['log__sum'] or 0
 
+        moneyStats = {}  # Build data for money dashboard
+
+        todayPayments = Payment.objects.filter(spent=date.today()).aggregate(Sum('amount'))['amount__sum'] or 0
+        moneyStats['today'] = todayPayments
+
+        monthPayments = Payment.objects.filter(spent__month=date.today().month).aggregate(Sum('amount'))['amount__sum'] or 0
+        moneyStats['month'] = monthPayments
+
         return render(request, 'root/home.html', {
             'blogStats': blogStats,
             'todoStats': todoStats,
+            'moneyStats': moneyStats,
         })
