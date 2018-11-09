fabe67374de6e2b4f6d6013b47ee8c22a5a0744d
Dmitry Komkov <drnecromant@gmail.com>
2018-11-09 21:11:45 +0100

    Added ShortUserSerializer

--- api/models/user.py
+++ api/models/user.py
@@ -3,5 +3,5 @@ from django.contrib.auth.models import User
 
 
 class UserIcon(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, related_name='user_icon', on_delete=models.CASCADE)
     icon = models.CharField(max_length=100)
--- api/serializers.py
+++ api/serializers.py
@@ -1,15 +1,29 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer, SlugRelatedField
 
 from django.contrib.auth.models import User
-from api.models import Post
+from api.models import Post, UserIcon
 
 
-class PostSerializer(serializers.HyperlinkedModelSerializer):
+class UserIconSerializer(ModelSerializer):
     class Meta:
-        model = Post
-        fields = ('title', 'body', 'created')
+        model = UserIcon
+        fields = ('icon',)
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
+
+class ShortUserSerializer(ModelSerializer):
+    user_icon = SlugRelatedField(read_only = True, slug_field = 'icon')
+
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'user_icon')
+
+class PostSerializer(ModelSerializer):
+    user = ShortUserSerializer(read_only = True)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'title', 'body', 'created', 'user')
