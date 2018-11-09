4a6a705316afc6d4d8325d54f65e87a7304a6cce
Dmitry Komkov <drnecromant@gmail.com>
2018-11-09 01:46:20 +0100

    Added CORS_ORIGIN_ALLOW_ALL

--- FireGolem/settings_prod.py
+++ FireGolem/settings_prod.py
@@ -3,7 +3,8 @@ from settings import *
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'firegolem.perforator.xyz']
 
 # Allow frontend access
-CORS_ORIGIN_WHITELIST = ['stormgolem.perforator.xyz']
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = ['stormgolem.perforator.xyz:443']
 
 DEBUG = False
 
