ef885e4fc23596cc96501a2f231394a39873b581
Dmitry Komkov <drnecromant@gmail.com>
2018-11-18 11:30:25 +0100

    simplify blogview

--- api/views.py
+++ api/views.py
@@ -13,24 +13,12 @@ class BlogView(ModelViewSet):
     queryset = Post.objects.all().order_by('-created')
     pagination_class = CustomPagination
 
-    def get(self, request, *args, **kwargs):
-        page_size = request.GET.get('page_size')           # Get custom page size from request params
-        if page_size:
-            self.pagination_class.page_size = page_size    # Set custom page size for paginator
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        # FIXME: do not completely delete the post
-        return self.destroy(request, *args, **kwargs)
+    def perform_destroy(self, instance):
+        instance.deleted = True
+        instance.save()
 
 class CurrentUserView(APIView):
 
