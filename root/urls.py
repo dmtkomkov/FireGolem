523d296cd196dcbebc24d21fb82905be8c5b9223
Dmitry Komkov <drnecromant@gmail.com>
2019-01-20 12:03:36 +0100

    Added refresh token point

--- root/urls.py
+++ root/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 from django.contrib.auth.views import login, logout
 from rest_framework_jwt.views import obtain_jwt_token
+from rest_framework_jwt.views import refresh_jwt_token
 from .views import RootView
 
 urlpatterns = [
     url(r'^$', RootView.as_view(), name='home'),
     url(r'^auth$', obtain_jwt_token, name='auth'),
+    url(r'^refresh$', refresh_jwt_token, name='refresh'),
     url(r'^login$', login, {'template_name': 'root/login.html', 'extra_context': {'title': 'Login'}}, name='login'),
     url(r'^logout$', logout, {'next_page': '/login'}, name='logout'),
 ]
