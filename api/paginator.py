f6b06aa952d141aa496350e161b51507968adf73
Dmitry Komkov <drnecromant@gmail.com>
2020-04-13 16:47:24 +0200

    Set default number of page 10 for blog

--- api/paginator.py
+++ api/paginator.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 
 class CustomPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 10
 
     def get_paginated_response(self, data):
         return Response({
