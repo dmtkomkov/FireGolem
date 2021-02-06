408f72986f29ea1d6abc34fb5e36be91c529a633
Dmitry Komkov <drnecromant@gmail.com>
2021-02-06 20:03:51 +0100

    api test for blog (get, put)

--- api/tests.py
+++ api/tests.py
@@ -24,15 +24,15 @@ class BlogTests(APITestCase):
         # check
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(Post.objects.count(), 1)
-        db_post = Post.objects.get()
+        db_post = Post.objects.first()
         self.check_post(db_post)
         self.assertEqual(db_post.deleted, False)
 
     def test_delete_post(self):
         # init
         url = reverse('api:post-detail', args=[1])
-        user = User.objects.get(username=self.username)
-        Post.objects.create(user=user, **self.test_post).save()
+        db_user = User.objects.get(username=self.username)
+        Post.objects.create(user=db_user, **self.test_post)
         # action
         response = self.client.delete(url, format='json')
         # check
@@ -43,7 +43,41 @@ class BlogTests(APITestCase):
         self.check_post(deleted_post)
         self.assertEqual(deleted_post.deleted, True)
 
-    def check_post(self, post):
-        self.assertEqual(post.title, self.title)
-        self.assertEqual(post.body, self.body)
+    def test_update_post(self):
+        # init
+        url = reverse('api:post-detail', args=[1])
+        db_user = User.objects.get(username=self.username)
+        Post.objects.create(user=db_user, **self.test_post)
+        new_test_post = {'title': 'new title', 'body': 'new body'}
+        # action
+        response = self.client.put(url, new_test_post, format='json')
+        # check
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        db_post = Post.objects.first()
+        self.check_post(db_post, 'new title', 'new body')
+        self.assertEqual(db_post.deleted, False)
+
+    def test_list_post(self):
+        # init
+        post_count = 10
+        url = reverse('api:post-list')
+        db_user = User.objects.get(username=self.username)
+        db_posts = [Post(title='Title %s' % i, body='Body %s' % i, user=db_user) for i in range(post_count)]
+        Post.objects.bulk_create(db_posts)
+        # action
+        response = self.client.get(url, format='json')
+        # check
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['count'], post_count)
+        self.assertEqual(response.data['page_count'], 1)
+        self.assertEqual(response.data['page_number'], 1)
+        self.assertEqual(response.data['page_size'], 100)
+        for i, post in enumerate(response.data['results']):
+            self.assertEqual(post['id'], i + 1)
+            self.assertEqual(post['title'], 'Title %s' % i)
+            self.assertEqual(post['body'], 'Body %s' % i)
+
+    def check_post(self, post, title=title, body=body):
+        self.assertEqual(post.title, title)
+        self.assertEqual(post.body, body)
         self.assertEqual(post.user.username, self.username)
